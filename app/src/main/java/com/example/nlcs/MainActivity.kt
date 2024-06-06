da1e4f42f7f83f1b5cca514b3152a07e4ff3ed19
Nguyen Quang Vinh <dev.nguyenquangvinh@gmail.com>
2024-06-06 22:16:37 +0700

    Adding the modified files git failed to add

--- app/src/main/java/com/example/nlcs/MainActivity.kt
+++ app/src/main/java/com/example/nlcs/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.nlcs
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.nlcs.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -14,6 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,14 +28,59 @@ class MainActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         firebaseAuth = Firebase.auth
 
-        // Test Click listener for log out button
-        binding.logOutTestButotn.setOnClickListener {
-            firebaseAuth.signOut()
-            val intent = Intent(this, LogInActivity::class.java)
-            startActivity(intent)
-            Toast.makeText(this, "Logged Out", Toast.LENGTH_SHORT).show()
-            finish()
+        setSupportActionBar(binding.toolbar)
+
+        drawerLayout = binding.drawerLayout
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.toolbar.setNavigationOnClickListener{
+            if(drawerLayout.isDrawerOpen(binding.navigationView)){
+                drawerLayout.closeDrawer(binding.navigationView)
+            }else{
+                drawerLayout.openDrawer(binding.navigationView)
+            }
         }
 
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId){
+                R.id.nav_logout -> {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    firebaseAuth.signOut()
+                    val intent = Intent(this, LogInActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(this, "Logged Out", Toast.LENGTH_SHORT).show()
+                    finish()
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed(){
+                if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }else{
+                    finish()
+                }
+            }
+        })
+
+        // Test Click listener for log out button
+//        binding.logOutTestButton.setOnClickListener {
+//            firebaseAuth.signOut()
+//            val intent = Intent(this, LogInActivity::class.java)
+//            startActivity(intent)
+//            Toast.makeText(this, "Logged Out", Toast.LENGTH_SHORT).show()
+//            finish()
+//        }
+
     }
+
+//    private fun isCurrentActivity(activityClass: Class<*>): Boolean {
+//        return activityClass == this::class.java
+//    }
 }
