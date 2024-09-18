1316999c0bccce128909b91f8ed07455e2a4f00d
Dung Ho <hoducdung3103@gmail.com>
2024-09-18 10:58:30 +0700

    First commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "NCKH"
 include(":app")
+include (":cardstackview")
  
